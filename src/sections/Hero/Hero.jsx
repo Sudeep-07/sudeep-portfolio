33391454ac729e8b6d52462be438df490768f3ad
sudeep v patil <76834928+Sudeep-07@users.noreply.github.com>
2025-04-14 18:38:41 +0530

    new changes

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,7 +8,7 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/sudeep_patil[2].pdf';
+import CV from '../../assets/Sudeep_resume final.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
@@ -25,7 +25,7 @@ function Hero() {
         <img
           src={heroImg}
           className={styles.hero}
-          alt="Profile picture of Harris Johnsen"
+          alt="Profile picture of Sudeep Patil"
         />
         <img
           className={styles.colorMode}
